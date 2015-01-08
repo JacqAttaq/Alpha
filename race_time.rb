ed0e1aa20da5424d18b63070f8b0797fe6720283
Jackie Tremblay <jackietremblayresponsibleadult@gmail.com>
2015-01-08 18:01:51 -0500

    And they're off

---
+++ race_time.rb
@@ -0,0 +1,43 @@
+# start game
+# draw race track, finish line
+# make horses
+# set loop for random horse progression
+# insert cheat code
+
+#win code - winners circle
+
+_______________________________________
+									   |
+									   |	
+_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _|
+									   |
+									   |
+_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _| 
+									   |
+									   |
+_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _| 
+									   |
+									   |
+_______________________________________|
+
+
+move= gets.chomp
+move= rand (1 .. 10)
+
+class Horse
+
+attr_accessor:name
+attr_accessor:breed
+attr_accessor:age
+attr_accessor:hand_height
+attr_accessor:record
+
+horse_one: "Pretty Penny"
+horse_two: "Smelly Melly"
+horse_three: "Krazy Karl"
+horse_four: "Bangers and Mash"
+
+"PP /--/P"
+"SM /--/P"
+"KK /--/P"
+"BandM /--/P"
