fe5b9a116eba1a706c10afe1ba807d91195d6e76
Jackie Tremblay <jackietremblayresponsibleadult@gmail.com>
2015-01-09 00:20:00 -0500

    The Midnight Coder

--- race_time.rb
+++ race_time.rb
@@ -1,3 +1,6 @@
+
+puts lineup
+
 # start game
 # draw race track, finish line
 # make horses
@@ -6,38 +9,76 @@
 
 #win code - winners circle
 
-_______________________________________
-									   |
-									   |	
-_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _|
-									   |
-									   |
-_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _| 
-									   |
-									   |
-_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _| 
-									   |
-									   |
-_______________________________________|
+# _______________________________________
+# 									   |
+# 									   |	
+# _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _|
+# 									   |
+# 									   |
+# _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _| 
+# 									   |
+# 									   |
+# _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _| 
+# 									   |
+# 									   |
+# _______________________________________|
 
 
-move= gets.chomp
-move= rand (1 .. 10)
+# move= gets.chomp
+# move= rand (1 .. 10)
 
 class Horse
 
-attr_accessor:name
-attr_accessor:breed
-attr_accessor:age
-attr_accessor:hand_height
-attr_accessor:record
+	attr_accessor:name
+	attr_accessor:breed
+	attr_accessor:age
+	attr_accessor:hand_height
+	attr_accessor:record
+	attr_accessor:icon
+	attr_accessor:@@lineup
 
-horse_one: "Pretty Penny"
-horse_two: "Smelly Melly"
-horse_three: "Krazy Karl"
-horse_four: "Bangers and Mash"
+	end	
+		def initialize
+			@name: "Elvis"
+			@breed: "Arbaian"
+			@age: "27"
+			@hand_height: "9"
+			@record: "0-20"
+			@@lineup = []
+		end	
+	
 
-"PP /--/P"
-"SM /--/P"
-"KK /--/P"
-"BandM /--/P"
+	lineup = Horse.new
+	Penny.name = "Pretty Penny"
+	Penny.breed = "Thoroughbred"
+	Penny.age = "7"
+	Penny.hand_height= "12"
+	Penny.record = "12-3"
+	Penny.icon = "PP `/--/%"
+
+	lineup = Horse.new
+	Melly.name: "Smelly Melly"
+	Melly.breed: "Quarter Horse"
+	Melly.age: "9"
+	Melly.hand_height: "8"
+	Melly.record: "7-9"
+	Melly.icon: "SM `/--/%"
+
+	lineup = Horse.new	
+	Karl.name: "Krazy Karl"
+	Karl.breed: "Percheron"
+	Karl.age: "4"
+	Karl.hand_height: "19"
+	Karl.record: "3-7"
+	Karl.icon: "KK `/--/%"
+
+	lineup = Horse.new
+	BNM.name: "Bangers and Mash"
+	BNM.breed: "Mule"
+	BNM.age: "12"
+	BNM.hand_height: "12"
+	BNW.record: "0-12"
+	BMW.icon: "BandM `/--/%"
+end
+
+
